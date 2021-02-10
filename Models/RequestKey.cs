2946bcfd3849cb239aa6540d5963d24d76764895
Dolu <doludev@protonmail.com>
2021-02-10 18:51:09 +0100

    Add model validation

--- Models/RequestKey.cs
+++ Models/RequestKey.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NostrBridge.Models
 {
     public class RequestKey
     {
+        [Required]
         public IEnumerable<string> Relays { get; set; }
+        [Required]
         public string PubKey { get; set; }
         public short Limit { get; set; } = 5;
         public int[] Kinds { get; set; } = new int[] { 1 };
